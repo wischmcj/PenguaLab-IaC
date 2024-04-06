e333c84f69cc4d4558aebb6c8e5e7e31daf96fc8
Collin <cjwischmeyer@gmail.com>
2024-04-06 11:15:23 -0400

    first commit

--- main.tf
+++ main.tf
@@ -1,21 +1,67 @@
 
 
-resource "docker_volume" "data" {
-  count = var.create_data_volume ? 1 : 0
+# resource "docker_volume" "data" {
+#   count = var.create_data_volume ? 1 : 0
 
-  name        = local.data_volume_name
-  driver      = var.data_volume_driver
-  driver_opts = var.data_volume_driver_opts
+#   name        = local.data_volume_name
+#   driver      = var.data_volume_driver
+#   driver_opts = var.data_volume_driver_opts
 
-  dynamic "labels" {
-    for_each = var.labels
-    iterator = label
-    content {
-      label = label.key
-      value = label.value
-    }
+#   dynamic "labels" {
+#     for_each = var.labels
+#     iterator = label
+#     content {
+#       label = label.key
+#       value = label.value
+#     }
+#   }
+# }
+
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  name = "nginx_pi"
+  # name  = var.nginx_container_name
+  image = docker_image.nginx.image_id
+  ports {
+    internal = 80
+    external = 8000
   }
 }
+
+resource "docker_image" "mariadb" {
+  #need older version to run on Arm v7
+  name         = "yobasystems/alpine-mariadb"
+  keep_locally = false
+}
+
+
+resource "docker_container" "db" {
+  name = "mariadb_pi"
+  image = docker_image.mariadb.image_id
+  restart = "always"
+  # network_mode = "wordpress_net"
+  env = [
+     "MYSQL_ROOT_PASSWORD=Alpha@Romeo@13",
+     "MYSQL_PASSWORD=Alpha@Romeo@13",
+     "MYSQL_USER=wischmcj",
+     "MYSQL_DATABASE=wischmcj"
+  ]
+  mounts {
+    type = "volume"
+    target = "/var/lib/mysql"
+    source = "db_data"
+  }
+}
+
+
+# resource "docker_volume" "maria_db_volume" {
+#   name = "shared_volume"
+# }
 
 resource "docker_network" "private_network" {
   name = "penguaworks"
--- providers.tf
+++ providers.tf
@@ -8,6 +8,10 @@ terraform {
 }
 
 provider "docker" {
+  #local on server
+  # host = "tcp://localhost:2375"
+  #remote
+  # host = "unix:///run/user/1000/docker.sock"
   host     = "ssh://wischmcj@192.168.0.94:22"
   ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
 
